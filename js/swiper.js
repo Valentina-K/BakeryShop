6ed716d12f125d9961416f79dfe08bf02cdd1acb
Valentшna-K <kvalentyna@hotmail.com>
2024-01-04 15:12:47 +0200

    done slider

---
+++ js/swiper.js
@@ -0,0 +1,29 @@
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 3,
+    },
+    1280: {
+      slidesPerView: 3,
+    },
+  },
+  navigation: {
+    nextEl: ".carousel-button.next",
+    prevEl: ".carousel-button.prev",
+  },
+  pagination: {
+    el: ".pagination",
+    bulletClass: "pagination__button",
+    bulletActiveClass: "pagination__button--active",
+    clickable: true,
+  },
+});
+
+console.dir(swiper);
+
+swiper.init();
